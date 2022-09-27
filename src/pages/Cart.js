20818354d7db06a9ff38f00e44ac0b4433f5075f
Jonathan Tuala <jetuala@gmail.com>
2022-09-27 14:13:47 -0700

    Some cart design/functionality

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,27 +1,67 @@
-import { Typography } from '@material-tailwind/react';
-import React, { useContext, useEffect } from 'react';
+import { Typography, Card, CardBody, Button } from '@material-tailwind/react';
+import React, { useContext, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import ShopContext from '../Context/ShopContext'
 
 export default function Cart() {
     const { cart } = useContext(ShopContext);
+    const [ total, setTotal ] = useState(0);
 
     useEffect(() => {
-        console.log(cart);
+        let cartTotal = 0;
+        cart.forEach(item => {
+            cartTotal += (item.product.fields.priceInGil * item.quantity)
+        })
+        setTotal(cartTotal);
     }, [cart])
 
     return (
+        (cart.length === 0) ? 
+        <div>
+            <Typography className="text-white">You currently have nothing in your cart.</Typography> 
+            <Typography className="text-white"><Link to="/search">Back to Search Page</Link></Typography>
+        </div>
+        :
         <div>
             <Typography className="text-white">You have the following items in your cart:</Typography>
             {cart.map(item => {
                 return (
                     <div key={item.product.fields.id}>
-                        <Typography className="text-white">{item.product.fields.name}</Typography>
-                        <Typography className="text-white">Quantity: {item.quantity}</Typography>
+                        <Card className="w-2/3">
+                            <CardBody className="grid grid-cols-5">
+                                <img
+                                    src={item.product.fields.icon.fields.file.url}
+                                    alt="img-blur-shadow"
+                                    className=""
+                                />
+                                <div>
+                                    <Typography>{item.product.fields.name}</Typography>
+                                </div>
+                                <div>
+                                    <Typography>Price in gil:</Typography>
+                                    <Typography>{item.product.fields.priceInGil}</Typography>
+                                </div>
+                                <div>
+                                    <Button>-</Button>
+                                    <Typography>Quantity: {item.quantity}</Typography>
+                                    <Button>+</Button>
+                                </div>
+                                <div>
+                                    <Typography>Total:</Typography>
+                                    <Typography>{item.product.fields.priceInGil * item.quantity}</Typography>
+                                </div>
+                            </CardBody>
+                        </Card>
+
                     </div>
                 )
             })}
-            <Typography className="text-white"><Link to="/search">Search Page</Link></Typography>
+            <Card className="w-2/3">
+                <CardBody>
+                    <Typography>Total: {total}</Typography>
+                </CardBody>
+            </Card>
+            <Typography className="text-white"><Link to="/search">Back to Search Page</Link></Typography>
         </div>
     )
 }
